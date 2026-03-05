b22a3e06422ca0c973cebbff9ad27afe9971150e
Jerry <chimobijeremaih@gmail.com>
2026-03-05 23:25:18 +0100

    deleting food functionality added

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 
 function MyComponent(){
     const foodsList = ['apple', 'orange', 'pawpaw', 'cigarette', 'risla']
-    const [foods, setFood] = useState()
+    const [foods, setFood] = useState(foodsList)
     const addFood = () => {
         let newFood = document.getElementById('foodInput').value
-        setFood([...foodsList,  newFood])
+        setFood([...foods,  newFood])
+        document.getElementById('foodInput').value = ""
+    }
+
+    const deleteFood = (index) => {
+        setFood(foods.filter((_, i) => i !== index))
     }
     
-    const newArray = foodsList.map((food, index) => <li key={index}>{food}</li>)
+    const newArray = foods.map((food, index) => <li key={index} onClick={() => deleteFood(index)}>{food}</li>)
     return (
         <div>
             <h1>List of food</h1>
