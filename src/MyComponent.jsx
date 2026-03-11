a3e5c34f00147b9c6eb97c553d5169da41caafbe
Jerry <chimobijeremaih@gmail.com>
2026-03-11 19:33:13 +0100

    removes car from list

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,28 +1,42 @@
 import { useState } from 'react'
 
+
+
 function MyComponent(){
-    
-    const carsList = ["mercedes", "toyota", "Ferrari", "BMW"]
-    const [cars, setCar] = useState(carsList)
-    
+    const [year, setYear] = useState()
+    const [brand, setBrand] = useState()
+    const [model, setModel] = useState()
+
+    const [cars, setCar] = useState([])
+
+    const getYear = (e) => { setYear(e.target.value)}
+    const getBrand = (e) => { setBrand(e.target.value)}
+    const getModel = (e) => { setModel(e.target.value)}
+     
     const addCar = () => {
-        const newCar = document.getElementById('input').value;
+        const newCar = {year: year, model: model, brand: brand}
+
+        console.log(cars)
         setCar([...cars, newCar])
+    }
 
-        document.getElementById('input').value = "";
-
+    const removeCar = (index) => {
+        setCar(cars.filter((car, i) => i !== index))
     }
 
     return(
         <div>
             <h1>List of Cars</h1>
             <ul>
-                {cars.map((car, index) => <li key={index}>{car}</li>)}
+                {cars.map((car, index) => <li key={index} onClick={() => removeCar(index)}>{car.year} {car.model} {car.brand}</li>)}
             </ul>
-            <input type="text" id='input'/>
+            <input type="number" onChange={getYear} /><br />
+            <input type="text" onChange={getBrand}/> <br />
+            <input type="text" onChange={getModel}/><br />
             <button onClick={addCar}>Enter car</button>
         </div>
     )
 }
 
-export default MyComponent
+
+export default MyComponent;
