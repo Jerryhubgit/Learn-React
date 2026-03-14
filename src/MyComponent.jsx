302860922c0b24fa0cc32e70f293416ef4ceb7b3
Jerry <chimobijeremaih@gmail.com>
2026-03-14 16:51:45 +0100

    todo test completed

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,42 +1,40 @@
 import { useState } from 'react'
 
-
-
 function MyComponent(){
-    const [year, setYear] = useState()
-    const [brand, setBrand] = useState()
-    const [model, setModel] = useState()
+    const [task, setTask] = useState('')
+    const [tasksList, setList] = useState(['washing', 'cooking'])
 
-    const [cars, setCar] = useState([])
-
-    const getYear = (e) => { setYear(e.target.value)}
-    const getBrand = (e) => { setBrand(e.target.value)}
-    const getModel = (e) => { setModel(e.target.value)}
-     
-    const addCar = () => {
-        const newCar = {year: year, model: model, brand: brand}
-
-        console.log(cars)
-        setCar([...cars, newCar])
+    const getTask = (e) => {
+        let newTask = e.target.value; 
+        setTask(newTask)
     }
 
-    const removeCar = (index) => {
-        setCar(cars.filter((car, i) => i !== index))
+    const addTask = () => {
+        setList([...tasksList, task])
+        document.querySelector('input').value = ""
+        console.log(tasksList)
+    }
+
+    const deleteTask = (index) => {
+        setList(tasksList.filter((_, i) => i !== index))
     }
 
     return(
-        <div>
-            <h1>List of Cars</h1>
-            <ul>
-                {cars.map((car, index) => <li key={index} onClick={() => removeCar(index)}>{car.year} {car.model} {car.brand}</li>)}
-            </ul>
-            <input type="number" onChange={getYear} /><br />
-            <input type="text" onChange={getBrand}/> <br />
-            <input type="text" onChange={getModel}/><br />
-            <button onClick={addCar}>Enter car</button>
+        <div className="todoContainer">
+            <h1>Todo</h1>
+            <div className="input">
+                <input type="text" onChange={getTask} />
+                <button onClick={addTask}>Add item</button>
+            </div>
+            <div className="todo_list">
+                {tasksList.map((task,index) => 
+                            <div className="todo" onClick={() => deleteTask(index)} key={index} id={index}>
+                                <p>{task}</p>
+                                <button>delete</button>
+                            </div>)}
+            </div>
         </div>
     )
 }
 
-
-export default MyComponent;
+export default MyComponent
